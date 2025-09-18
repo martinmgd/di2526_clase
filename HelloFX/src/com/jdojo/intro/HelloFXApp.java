fa682f32dfffeb80153ba002bdd2f86c51e0749e
martinmgd <martin_mgd@hotmail.com>
2025-09-18 11:23:10 +0200

    Comienzo con parametros

--- HelloFX/src/com/jdojo/intro/HelloFXApp.java
+++ HelloFX/src/com/jdojo/intro/HelloFXApp.java
@@ -13,20 +13,22 @@ public class HelloFXApp extends Application{
 	}
 	@Override
 	public void start(Stage stage)throws Exception{
-		//Establecer un título para o escenario
-		stage.setTitle("Aplicación JavaFX con unha escena");
-		//Amosar o escenario
-		stage.show();
+		
 		//Creamos un nodo raíz VBox
 		VBox root = new VBox();
 		//Creamos un nodo de texto
 		Text msg = new Text("Hola JavaFX");
 		//Añadimos un nodo Text ó VBox como fillo
 		root.getChildren().add(msg);
+		
 		//Creamos unha escena especificando nodo e tamaño
 		Scene scene = new Scene(root, 300, 50);
 		//Asignamos a escena ó escenario
 		stage.setScene(scene);
+		//Establecer un título para o escenario
+		stage.setTitle("Aplicación JavaFX con unha escena");
+		//Amosar o escenario
+		stage.show();
 		
 	}
 }
